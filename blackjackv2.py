6838ed0b29b235ec73554c318cb050f4bb0d8ff3
Yash Purav <yashpurav@gmail.com>
2021-04-10 18:38:23 -0400

    added code, stil getting global glitch

--- blackjackv2.py
+++ blackjackv2.py
@@ -6,18 +6,31 @@ dealertotal=0
 playerbust= False
 dealerbust= False
 gametie= False
-def isBust():
-    if playertotal >21:
-        playerbust= True
-    if dealertotal >21:
-        dealerbust= True
+def importantStatements():
+    print ("Total number of dealer cards " + str(dealercardcount))
+    print ("Total value of dealer cards " + str(dealertotal))
+    print ("playercardcount " +str(playercardcount))
+    print ("playercardtotal " +str(playertotal))
+
+
+def getrandomcard():
+    return random.randint(1,13)
+
 def isTie():
     if playerbust==dealerbust:
         gametie= True
         print ("It's a tie!")
-def getrandomcard():
-    return random.randint(1,13)
-print (playercardcount)
+def dealerTurn():
+    global dealercardcount;
+    dealercardcount= dealercardcount+1
+    global dealertotal;
+    dealertotal= getrandomcard()+dealertotal
+dealerTurn()
+
+#Prints dealer card stats:
+print ("Total number of dealer cards " + str(dealercardcount))
+print ("Total value of dealer cards " + str(dealertotal))
+
 def hit():
     global playercardcount;
     playercardcount+=1
@@ -25,7 +38,33 @@ def hit():
     playertotal= getrandomcard()+playertotal
 user1= input ("insert name here   ")
 print ("generating random card for " +user1)
-#hit()
-print (playertotal)
-print (playercardcount)
-   
+hit()
+
+#Prints player card stats:
+print ("playercardcount " +str(playercardcount))
+print ("playercardtotal " +str(playertotal))
+
+
+def stand(): 
+    print ("you have have chosen to stand")
+def roundCheck():
+    global playertotal
+    if playertotal >21:
+        playerbust= True
+    global dealertotal
+    if dealertotal >21:
+        dealerbust= True
+    #put this at the end of the game: if isTie():
+        #print ("you tied!, you suck") 
+        #check exit game function
+    global playerbust
+    if playerbust:
+        print ("dealer wins, you suck")
+         #check exit game function
+    global dealerbust
+    if dealerbust:
+        print ("you win, congrats!")
+         #check exit function
+roundCheck()
+
+
